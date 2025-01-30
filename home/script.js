8cb5266199a7fb7bddbf76e055207419e32f4ed0
Anay0305 <anayg0305@gmail.com>
2025-01-31 04:47:02 +0530

    s

--- home/script.js
+++ home/script.js
@@ -1,5 +1,33 @@
 const API_URL = "http://192.168.1.13:5000";
 
+async function signup() {
+    const username = document.getElementById("signupUsername").value;
+    const password = document.getElementById("signupPassword").value;
+    
+    const res = await fetch(`${API_URL}/signup`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: 'include', 
+        body: JSON.stringify({ username, password })
+    });
+    const data = await res.json();
+    alert(data.message);
+}
+
+async function login() {
+    const username = document.getElementById("loginUsername").value;
+    const password = document.getElementById("loginPassword").value;
+    
+    const res = await fetch(`${API_URL}/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: 'include', 
+        body: JSON.stringify({ username, password })
+    });
+    const data = await res.json();
+    alert(data.message);
+}
+
 async function check() {
     const res = await fetch(`${API_URL}/status`, {
         method: "GET",
@@ -7,82 +35,5 @@ async function check() {
         credentials: "include"
     });
     const data = await res.json();
-    if (data.loggedIn) {}
-    else {
-        window.location.href = "/";
-    }
+    alert(data.loggedIn);
 }
-check()
-
-function hexToRgb(hex) {
-  hex = hex.replace(/^#/, '');
-  let r = parseInt(hex.substring(0, 2), 16);
-  let g = parseInt(hex.substring(2, 4), 16);
-  let b = parseInt(hex.substring(4, 6), 16);
-  return `rgba(${r}, ${g}, ${b}, 0.7)`;
-}
-const root = document.querySelector(":root");
-const themeToggle = document.querySelector("#navbar__logo");
-let currentTheme = 0;
-themeToggle.addEventListener("click", () => {
-  fetch("database.json")
-      .then(response => response.json())
-      .then(data => {
-        const themes = data.themes;
-        currentTheme++;
-        if (currentTheme == themes.length) {
-          currentTheme = 0;
-        }
-        root.style.setProperty("--main-accent", themes[currentTheme]);
-        root.style.setProperty("--secondary", hexToRgb(themes[currentTheme]));
-      });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("database.json")
-      .then(response => response.json())
-      .then(data => {
-        appendTimelineMessages(data.Papers, "memorable");
-      });
-    });
-
-function AttemptedOrNot(paperid) {
-    return false
-}
-
-function appendTimelineMessages(messages, containerId) {
-    const container = document.getElementById(containerId);
-    if (!container) return;
-    const timelineList = container.querySelector("ul");
-    messages.forEach(message => {
-      const listItem = document.createElement("li");
-      const h1 = message.title ? `<h1>${message.title}</h1>` : "";
-      const button = AttemptedOrNot(message.id) ? `<div class="reattempt-btn" id="Start ${message.id}">Re-Attempt Test</div>
-        <div class="result-btn" id="Res ${message.id}">View Result</div>` : `<div class="start-btn" id="Start ${message.id}">Start Test</div>`;
-      const messageContent = `
-        <div class="timeline__content">
-          ${h1}
-          <p>${message.message}</p>
-        </div>
-        ${button}
-      `;
-      listItem.innerHTML = messageContent;
-      timelineList.appendChild(listItem);
-    });
-    messages.forEach(message => {
-        const startBtn = document.getElementById(`Start ${message.id}`);
-        const reattemptBtn = document.getElementById(`ReAttempt-${message.id}`);
-
-        if (startBtn) {
-            startBtn.addEventListener("click", () => {
-              window.location.href = `/test?testid=${message.id}`;
-            });
-        }
-
-        if (reattemptBtn) {
-            reattemptBtn.addEventListener("click", () => {
-                window.location.href = `/test?testid=${message.id}`;
-            });
-        }
-    });
-  }
